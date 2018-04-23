c336cb5717593e807cc2b14da60def7866c3aa35
helloMrzhangjk <246873354@qq.com>
2018-04-23 14:44:47 +0800

    add document.js

---
+++ static/EVmiRNA/js/document.controller.js
@@ -0,0 +1,8 @@
+"use strict";
+
+angular.module("EVmiRNA")
+	.controller("DocumentController",DocumentController);
+
+function DocumentController($scope){
+	console.log("DocumentController loaded");
+}
